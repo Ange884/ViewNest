feced31b9ffc9944d3719a35cdbc5170571fac07
Ange <ishimwenzizaangell@gmail.com>
2025-11-21 19:56:06 +0200

    .

--- components/welcome.jsx
+++ components/welcome.jsx
@@ -4,9 +4,11 @@ import { View, Image, Text, StyleSheet, TouchableOpacity } from "react-native";
 export default function WelcomeScreen({navigation}) {
   return (
     <View style={styles.container}>
-      <Image source={require('../assets/images/landing.png')} style={styles.image} />
+        <View style={styles.footerContainer}>
+     
+      <Image source={require('../assets/images/Vn.png')} style={styles.image} />
 
-      <Text style={styles.title}>ViewNest</Text>
+      <Text style={styles.Text}>Welcome <span style={styles.highlight}>User</span></Text>
 
       <TouchableOpacity
         onPress={() => navigation.navigate("welcome")}
@@ -16,10 +18,18 @@ export default function WelcomeScreen({navigation}) {
         <Text style={styles.buttonText}>Get Started</Text>
       </TouchableOpacity>
 
+      <TouchableOpacity
+        onPress={() => navigation.navigate("welcome")}
+          style={styles.buttonBottom}
+          activeOpacity={0.8}
+         >
+         <Text style={styles.buttonTextBottom}>Sign Up</Text>
+        </TouchableOpacity>
+
+
+
       {/* Footer */}
-      <View style={styles.footerContainer}>
-        <Text style={styles.footerText}>Created by Nziza Ange</Text>
-      </View>
+       </View>
     </View>
   );
 }
@@ -32,6 +42,12 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     paddingHorizontal: 20,
     position: "relative",
+  },
+  Text:{
+       color:"#555",
+       fontSize:16,
+       marginBottom:20,
+
   },
   image: {
     resizeMode: "contain",
@@ -46,8 +62,9 @@ const styles = StyleSheet.create({
   },
   button: {
     backgroundColor: "#242424",
-    paddingVertical: 14,
+    paddingVertical: 10,
     paddingHorizontal: 40,
+    width:350,
     borderRadius: 25,
     alignItems: "center",
     justifyContent: "center",
@@ -56,12 +73,37 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.2,
     shadowRadius: 4,
     elevation: 5, // Android shadow
-    marginBottom: 20,
+    marginBottom: 7,
   },
+ buttonBottom: {
+  backgroundColor: "#fff",       // white fill
+  borderWidth: 1,                // visible border
+  borderColor: "#ff8989ff",        // red border
+  paddingVertical: 10,
+  paddingHorizontal: 40,
+  width: 350,
+  borderRadius: 25,
+  alignItems: "center",
+  justifyContent: "center",
+  shadowColor: "#000",
+  shadowOffset: { width: 0, height: 3 },
+  shadowOpacity: 0.1,            // subtle shadow
+  shadowRadius: 4,
+  elevation: 3,
+  marginBottom: 15,
+},
+buttonTextBottom: {
+  color: "#E80000",              // red text matching border
+  fontSize: 16,
+},
+
+highlight:{
+    color:"#E80000",
+},
+
   buttonText: {
     color: "#fff",
-    fontSize: 18,
-    fontWeight: "600",
+    fontSize: 16,
   },
   footerContainer: {
     position: "absolute",
