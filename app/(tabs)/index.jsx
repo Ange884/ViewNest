11d2f525315d205a2a324a5a1e554442a4492bb6
Ange <ishimwenzizaangell@gmail.com>
2025-11-21 11:00:46 +0200

    .

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -9,7 +9,6 @@ export default function MainScreen() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -17,10 +16,5 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
     color:"#000"
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
   },
 });
--- components/landing.jsx
+++ components/landing.jsx
@@ -1,32 +1,61 @@
 import React from "react";
-import { View,Image,Text} from "react-native-web";
-import { StyleSheet } from "react-native";
-export default function LandingScreen(){
-    return(
-        <View style={styles.container}>
-            <Image source={require('../assets/images/landing.png')} />
-            <Text style={styles.text}>ViewNest</Text>
-            <Text style={styles.footerText}>Created by Nziza Ange</Text>
-        </View>
-);
+import { View, Image, Text, StyleSheet,TouchableOpacity } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+
+export default function LandingScreen({navigation}) {
+  return (
+    <View style={styles.container}>
+      <Image source={require('../assets/images/landing.png')} />
+      <Text style={styles.text}>ViewNest</Text>
+
+      {/* Footer */}
+      <View style={styles.footerContainer}>
+        <Text style={styles.footerText}>Created by Nziza Ange</Text>
+        <TouchableOpacity onPress={() =>navigation.navigate("welcome")} style={styles.button}>
+          <Text style={[styles.buttonText, { marginTop: 5 }]}>
+            Get Started
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
 
-const styles =StyleSheet.create({
-       container:{
-        flex:1,
-        alignItems:'center',
-        justifyContent:'center',
-       },
-         text:{
-            fontSize:30,
-            fontWeight:'bold',
-            color:'#000'
-         },
-         footerText:{
-            justifyContent:'flex-end',
-            fontSize:13,
-            color:'#555',
-            marginTop:90,
-            bottom:0
-         }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    position: "relative", // allows absolute positioning of footer
+  },
+  text: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "#000",
+  },
+  footerContainer: {
+    position: "absolute",
+    bottom: 20, // distance from bottom
+    width: "100%",
+    alignItems: "center",
+  },
+  footerText: {
+    fontSize: 13,
+    color: "#616060ff",
+  },
+   button: {
+    marginTop: 10,
+    backgroundColor: "#242424",
+    paddingVertical: 10, // vertical padding
+    paddingHorizontal: 25, // horizontal padding
+    borderRadius: 20,
+    alignItems: "center", // centers text horizontally
+    justifyContent: "center", // centers text vertically
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    textAlign: "center", // just in case
+  },
+
 });
