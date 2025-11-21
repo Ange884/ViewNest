8563186cfb4ac6951d1bd7c8b9433d0462450e3d
Ange <ishimwenzizaangell@gmail.com>
2025-11-21 09:09:24 +0200

    .

--- components/landing.jsx
+++ components/landing.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { View,Image } from "react-native-web";
+import { View,Image,Text} from "react-native-web";
 import { StyleSheet } from "react-native";
 export default function LandingScreen(){
     return(
         <View style={styles.container}>
             <Image source={require('../assets/images/landing.png')} />
+            <Text style={styles.text}>ViewNest</Text>
+            <Text style={styles.footerText}>Created by Nziza Ange</Text>
         </View>
 );
 }
@@ -15,4 +17,16 @@ const styles =StyleSheet.create({
         alignItems:'center',
         justifyContent:'center',
        },
+         text:{
+            fontSize:30,
+            fontWeight:'bold',
+            color:'#000'
+         },
+         footerText:{
+            justifyContent:'flex-end',
+            fontSize:13,
+            color:'#555',
+            marginTop:90,
+            bottom:0
+         }
 });
