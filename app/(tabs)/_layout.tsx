b1a7e8cd60764265c8ce0278b5f6cf99d3a0dbd9
Ange <ishimwenzizaangell@gmail.com>
2025-11-21 07:35:22 +0200

    .

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,5 @@
-import React from 'react';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
+import { Tabs } from "expo-router";
 
-export default function TabLayout() {
-
-  return (
-    <></>
-  );
+export default function Layout() {
+  return <Tabs />;
 }
