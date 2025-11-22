fb96f11f47b60c9ce0530d22cbe212c8dd17dea3
Ange <ishimwenzizaangell@gmail.com>
2025-11-22 21:34:54 +0200

    .

--- components/Register2.jsx
+++ components/Register2.jsx
@@ -1,15 +1,13 @@
-import { Ionicons } from '@expo/vector-icons';
 import React from 'react';
 import { View, Text, StyleSheet,TextInput} from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
 export default function Register2Screen(){
     return(
         <View style={styles.container}>
-            <Image source={require('../assets/images/verifyOTP.png')} style={styles.image} />
+            <Image source={require('../assets/images/Group 1.png')} style={styles.image} />
             <Text style={styles.title}>Let's Get You<Text style={styles.highlight}>Started</Text></Text>
             <Text style={styles.subtitle}>What are You looking for exactly?Let's help you!</Text>
                <View style={styles.inputContainer}>
-      <Image source={require('../assets/images/home-outline.png')} style={styles.leftIcon} />
+               <Image source={require('../assets/images/house-building 3.png')} style={styles.leftIcon} />
 
       <TextInput
         style={styles.input}
@@ -17,10 +15,8 @@ export default function Register2Screen(){
         placeholderTextColor="#999"
       />
 
-      <Ionicons
-        name="chevron-down-outline"
-        size={20}
-        color="#999"
+      <Image
+       source ={require('../assets/images/Vector 11.png')}
         style={styles.rightIcon}
       />
     </View>
