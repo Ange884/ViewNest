7cb458343364448335ac8151d452705e8f906bd4
Ange <ishimwenzizaangell@gmail.com>
2025-11-22 21:28:16 +0200

    .

--- components/Register2.jsx
+++ components/Register2.jsx
@@ -1,14 +1,29 @@
+import { Ionicons } from '@expo/vector-icons';
 import React from 'react';
 import { View, Text, StyleSheet,TextInput} from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 export default function Register3(){
     return(
         <View style={styles.container}>
             <Image source={require('../assets/images/verifyOTP.png')} style={styles.image} />
             <Text style={styles.title}>Let's Get You<Text style={styles.highlight}>Started</Text></Text>
             <Text style={styles.subtitle}>What are You looking for exactly?Let's help you!</Text>
-            <TextInput style={styles.input} 
-            placeholder="Type of Building" 
-            placeholderTextColor="#999" />
+               <View style={styles.inputContainer}>
+      <Image source={require('../assets/images/home-outline.png')} style={styles.leftIcon} />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Type of Building"
+        placeholderTextColor="#999"
+      />
+
+      <Ionicons
+        name="chevron-down-outline"
+        size={20}
+        color="#999"
+        style={styles.rightIcon}
+      />
+    </View>
         </View>
     );
 }
