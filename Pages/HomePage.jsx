89b4b5a83d592b0b0bc2b87b4adb54eab70799ae
Ange <ishimwenzizaangell@gmail.com>
2025-11-24 20:34:00 +0200

    .

---
+++ Pages/HomePage.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { View, Image, Text, StyleSheet, TouchableOpacity, TextInput, ActivityIndicator,ScrollView } from "react-native";
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_600SemiBold,
+  Poppins_700Bold
+} from "@expo-google-fonts/poppins";
+export default function HomePageScreen(){
+    return (
+      <ScrollView style={{flex:1}}
+      contentContainerStyle={{
+        flexGrow:1,
+        paddingHorizontal:10,
+        paddingVertical:30,
+      }}
+      ></ScrollView>
+    );
+}
