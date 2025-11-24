ccd148a2b806e52960ea05dbdf58b72da0b0e31f
Ange <ishimwenzizaangell@gmail.com>
2025-11-24 19:48:55 +0200

    .

---
+++ components/login.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import React from "react";
+import { View, Image, Text, StyleSheet, TouchableOpacity, TextInput, ActivityIndicator,ScrollView } from "react-native";
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_600SemiBold,
+  Poppins_700Bold
+} from "@expo-google-fonts/poppins";
+
+export default function LoginScreen(){
+        
+
+}
